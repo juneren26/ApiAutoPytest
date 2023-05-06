824aa255ac45e3a100546448afb45ba77d78f038
juneren26 <70616792+juneren26@users.noreply.github.com>
2023-05-06 11:25:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import os
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(['-v', '--alluredir', './result'])
-    # pytest.main(['-v', '--alluredir', './result', '--clean-alluredir'])
-    # os.system('allure generate ./result/ -o ./report_allure/ --clean')
+    # pytest.main(['-v', '--alluredir', './result'])
+    pytest.main(['-v', '--alluredir', './result', '--clean-alluredir'])
+    os.system('allure generate ./result/ -o ./report_allure/ --clean')
